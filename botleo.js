7b72e81fc98b2679d53b3c8b042e47068a21b5e2
Dan3s <daniel.guzman1@correo.icesi.edu.co>
2020-11-23 12:50:24 -0500

    Update botleo.js

--- botleo.js
+++ botleo.js
@@ -18,7 +18,7 @@ client.on("ready", () => {
 
 client.on("message", (msg) => {
 
-  if(msg.member.roles.cache.some(r=>["Estudiante"].includes(r.name))){
+  if(msg.member.roles.cache.some(r=>["Admin"].includes(r.name))){
 
     if (msg.content.includes("llegado")) {
       msg.reply("si te refieres a por qué no te ha llegado tu texto retroalimentado, recuerda que te llegará a tu correo en horas de la tarde. A veces tenemos mucho trabajo y tardamos un poquito más :(").then(ms => {
@@ -39,6 +39,14 @@ client.on("message", (msg) => {
     else if (msg.content.includes("link") && !(msg.content.includes("Moodle") || msg.content.includes("moodle"))) {
       msg.reply("si el link al canal que te dio el monitor no funciona, puedes entrar directamente al canal de voz de tu monitor asignado. Dicho canal lo encuentras en la parte izquierda, busca su nombre y da click en él. Si sigues teniendo problemas espera que un monitor te asista.").then(ms => {
         ms.delete({timeout: 120000})
+      })
+      .catch(); /*Used for error handling*/
+     
+    }
+
+    else if (msg.content.includes("solicitar cita") || msg.content.includes("solicitar una cita") || msg.content.includes("agendar una cita")) {
+      msg.reply("Para agilizar el agendamiento de citas recuerda escribir: \n**1. **Su nombre y apellido (por si no es claro en tu nickname). \n**2.** Uno o varios horarios que les puedan servir (los horarios están en #horarios). \n**3. **El motivo de la cita (revisar un ensayo, ayuda en oralidad, etc.). \n**4. **Si quieren con un monitor en especifico (esta es opcional).\n**5. **La materia. \nEste mensaje se autodestruirá en 1 minuto...").then(ms => {
+        ms.delete({timeout: 60000})
       })
       .catch(); /*Used for error handling*/
      
